d255267b837531db7e5779f1d40a45316417260f
Daniel Bastidas <dybastidas95@gmail.com>
2026-06-18 22:05:39 +0000

    commit-02-implement JSON

--- src/main/java/quarkus/TemperaturaResource.java
+++ src/main/java/quarkus/TemperaturaResource.java
@@ -1,12 +1,34 @@
 package quarkus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.quarkus.runtime.Application;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/temperaturas")
 public class TemperaturaResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public List<Temperatura> list(){
+        return Arrays.asList(
+            new Temperatura("Bogotá", 28, 15),
+            new Temperatura("Neiva", 40, 20),
+            new Temperatura("Medellín", 15, 28)
+        );
+
+    }
+
+
+    @GET
+    @Path("/una")
+    @Produces(MediaType.APPLICATION_JSON)
     public Temperatura medicion(){
         return new Temperatura("Bogotá", 15, 28); 
     }
